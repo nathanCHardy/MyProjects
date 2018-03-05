949e246521df7cb4d098bc69cfae4ab1d3974d3f
nathanCHardy <37088582+nathanCHardy@users.noreply.github.com>
2018-03-05 16:18:15 -0700

    C# morse code

---
+++ Morse Code Converter/Morse Code Converter/Morse Code Converter/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Morse_Code_Converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSentence = new System.Windows.Forms.TextBox();
+            this.lblMorse = new System.Windows.Forms.Label();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtSentence
+            // 
+            this.txtSentence.Location = new System.Drawing.Point(29, 35);
+            this.txtSentence.Name = "txtSentence";
+            this.txtSentence.Size = new System.Drawing.Size(227, 20);
+            this.txtSentence.TabIndex = 0;
+            // 
+            // lblMorse
+            // 
+            this.lblMorse.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblMorse.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMorse.Location = new System.Drawing.Point(29, 74);
+            this.lblMorse.Name = "lblMorse";
+            this.lblMorse.Size = new System.Drawing.Size(227, 87);
+            this.lblMorse.TabIndex = 1;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(12, 211);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(75, 23);
+            this.btnConvert.TabIndex = 2;
+            this.btnConvert.Text = "Convert ";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(169, 211);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.lblMorse);
+            this.Controls.Add(this.txtSentence);
+            this.Name = "Form1";
+            this.Text = "Morse Code Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSentence;
+        private System.Windows.Forms.Label lblMorse;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Morse Code Converter/Morse Code Converter/Morse Code Converter/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Morse_Code_Converter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string[] MorseCode = new string[40] { " ", " --..--" , ".-.-.-" , "..--.." , "-----", ".----",
+            "..---", "...--", "....-"," .....", "-....", "--...", "---..", "----.", ".-", "-...", "-.-."," -..",
+        ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.",
+        "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."};
+        string morseTranslation = "";
+        char[] alphaNumeric = new char[] { ' ', ',', '.', '?', '0', '1','2', '3', '4', '5', '6',
+            '7', '8', '9','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            morseTranslation = "";
+            string userMessage = txtSentence.Text.ToLower();
+
+            foreach (char val in userMessage)
+            {
+                for (int x = 0; x < alphaNumeric.Length - 1; x++)
+                {
+                    if (val == ' ')
+                    {
+                        morseTranslation += " ";
+                        break;
+                    }
+                    else if (val == alphaNumeric[x])
+                    {
+                        morseTranslation += MorseCode[x];
+                        break;
+                    }
+                }
+            }
+            lblMorse.Text = morseTranslation;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
